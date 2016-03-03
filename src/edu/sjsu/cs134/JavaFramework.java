e75a9e431580729e8da624e23d0ad7398a98560a
xephos049 <joseph.joe93@gmail.com>
2016-03-02 20:33:07 -0800

    Working game
    camera movement and player movement.

---
+++ src/edu/sjsu/cs134/JavaFramework.java
@@ -0,0 +1,429 @@
+/**
+ * Created by jared on 2/9/16.
+ */
+package edu.sjsu.cs134;
+
+//import javax.media.nativewindow.WindowClosingProtocol;
+//import javax.media.opengl.*;
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLException;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.nativewindow.*;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public class JavaFramework {
+	// Set this to true to force the game to exit.
+	private static boolean shouldExit;
+
+	// The previous frame's keyboard state.
+	private static boolean kbPrevState[] = new boolean[256];
+
+	// The current frame's keyboard state.
+	private static boolean kbState[] = new boolean[256];
+
+	// Position of the sprite.
+	private static int[] spritePos = new int[2];
+
+	// Texture for the sprite.
+	private static int spriteTex1, spriteTex2, spriteTexAlt1, spriteTexAlt2, background, background_alt;
+	private static int para1, para2, para3, para4, para5, para6;
+	private static int[] parallaxTex = new int[6];
+	private static int[] spriteidleR = new int[2];
+	private static int[] spriteidleL = new int[2];
+	public static int changedir = 0;
+	static Sprite spr;
+	static int parallexFrame;
+	static int xwindow, ywindow, xbg, ybg, xmax, ymax;
+	static int time;
+	static int[][] tiles;
+
+	// Size of the sprite.
+	private static int[] spriteSize = new int[] { 50, 50 };
+
+	static GL2 gl;
+	static Background para, bac;
+	static Camera cam;
+
+	static// Direction sprite is facing
+	boolean faceRight = true;
+
+	public static void main(String[] args) {
+		GLProfile gl2Profile;
+		xmax = 1280;
+		cam = new Camera();
+		bac = new Background();
+		para = new Background();
+		spr = new Sprite();
+		time = 50;
+		parallexFrame = 0;
+		cam.x = 0;
+		cam.y = 0;
+		bac.w = 40;
+		bac.y = -120;
+		xwindow = 640;
+		ywindow = 480;
+		// final int ysprite = 50;
+		tiles = new int[50][50];
+		spr.x = (xwindow / 2) - (bac.w / 2);
+		spr.y = 405;
+
+		spritePos[0] = (xwindow / 2) - (bac.w / 2);
+		// spritePos[1] = (ywindow / 2) - (ysprite / 2);
+		spritePos[1] = 405;
+
+		try {
+			// Make sure we have a recent version of OpenGL
+			gl2Profile = GLProfile.get(GLProfile.GL2);
+		} catch (GLException ex) {
+			System.out.println("OpenGL max supported version is too low.");
+			System.exit(1);
+			return;
+		}
+
+		// Create the window and OpenGL context.
+		GLWindow window = GLWindow.create(new GLCapabilities(gl2Profile));
+		window.setSize(640, 480);
+		window.setTitle("Java Framework");
+		window.setVisible(true);
+		window.setDefaultCloseOperation(WindowClosingProtocol.WindowClosingMode.DISPOSE_ON_CLOSE);
+		window.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent keyEvent) {
+				kbState[keyEvent.getKeyCode()] = true;
+			}
+
+			@Override
+			public void keyReleased(KeyEvent keyEvent) {
+				kbState[keyEvent.getKeyCode()] = false;
+			}
+		});
+
+		// Setup OpenGL state.
+		window.getContext().makeCurrent();
+		gl = window.getGL().getGL2();
+		gl.glViewport(cam.x, cam.y, 640, 480);
+		gl.glMatrixMode(GL2.GL_PROJECTION);
+		gl.glOrtho(0, 640, 480, 0, 0, 100);
+		gl.glEnable(GL2.GL_TEXTURE_2D);
+		gl.glEnable(GL2.GL_BLEND);
+		gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+
+		// Load the texture.
+		// spriteTex = glTexImageTGAFile(gl, "Mega-Man-transparent.tga",
+		// spriteSize);
+		// spriteTexAlt = glTexImageTGAFile(gl, "Mega-Man-transparent_alt.tga",
+		// spriteSize);
+		spriteTex1 = glTexImageTGAFile(gl, "ghostr1.tga", new int[] { 1000, 640 });
+		spriteTex2 = glTexImageTGAFile(gl, "ghostr2.tga", new int[] { 1000, 640 });
+		spriteTexAlt1 = glTexImageTGAFile(gl, "ghostl1.tga", new int[] { 1000, 640 });
+		spriteTexAlt2 = glTexImageTGAFile(gl, "ghostl2.tga", new int[] { 1000, 640 });
+		para1 = glTexImageTGAFile(gl, "parallax1.tga", new int[] { 1000, 640 });
+		para2 = glTexImageTGAFile(gl, "parallax2.tga", new int[] { 1000, 640 });
+		para3 = glTexImageTGAFile(gl, "parallax3.tga", new int[] { 1000, 640 });
+		para4 = glTexImageTGAFile(gl, "parallax4.tga", new int[] { 1000, 640 });
+		para5 = glTexImageTGAFile(gl, "parallax5.tga", new int[] { 1000, 640 });
+		para6 = glTexImageTGAFile(gl, "parallax6.tga", new int[] { 1000, 640 });
+		// background = glTexImageTGAFile(gl, "endless_background_small.tga",
+		// new int[] { xbg, ybg });
+		background = glTexImageTGAFile(gl, "starry_night.tga", new int[] { bac.w, bac.w });
+		background_alt = glTexImageTGAFile(gl, "starry_night_alt.tga", new int[] { bac.w, bac.w });
+		// walkway = glTexImageTGAFile(gl, "walkway.tga", new int[] { xbg, ybg
+		// });
+
+		spriteidleR[0] = spriteTex1;
+		spriteidleR[1] = spriteTex2;
+
+		spriteidleL[0] = spriteTexAlt1;
+		spriteidleL[1] = spriteTexAlt2;
+
+		parallaxTex[0] = para1;
+		parallaxTex[1] = para2;
+		parallaxTex[2] = para3;
+		parallaxTex[3] = para4;
+		parallaxTex[4] = para5;
+		parallaxTex[5] = para6;
+
+		for (int i = 0; i < tiles.length; i++)
+			for (int j = 0; j < tiles[i].length; j++) {
+				if (i == j)
+					tiles[i][j] = 1;
+				else
+					tiles[i][j] = 0;
+			}
+
+		// glDrawSprite(gl, spriteTex, spritePos[0], spritePos[1],
+		// spriteSize[0], spriteSize[1]);
+
+		// The game loop
+		while (!shouldExit) {
+			System.arraycopy(kbState, 0, kbPrevState, 0, kbState.length);
+
+			// Actually, this runs the entire OS message pump.
+			window.display();
+			if (!window.isVisible()) {
+				shouldExit = true;
+				break;
+			}
+
+			// Game logic.
+			if (kbState[KeyEvent.VK_ESCAPE]) {
+				shouldExit = true;
+			}
+
+			if (kbState[KeyEvent.VK_LEFT]) {
+				if (bac.x + cam.x < 0) {
+					para.x += 3;
+					bac.x += 5;
+				} else {
+
+				}
+				faceRight = false;
+			}
+
+			if (kbState[KeyEvent.VK_RIGHT]) {
+				if (bac.x + cam.x > -1000) {
+					para.x -= 3;
+					bac.x -= 5;
+				} else {
+
+				}
+				faceRight = true;
+			}
+
+			if (kbState[KeyEvent.VK_W]) {
+				if (cam.y + bac.y < 400) {
+					para.y++;
+					cam.y++;
+					spr.y++;
+				}
+			}
+			if (kbState[KeyEvent.VK_S]) {
+				if (cam.y + bac.y > 0) {
+					para.y--;
+					cam.y--;
+					spr.y--;
+				}
+			}
+			if (kbState[KeyEvent.VK_A]) {
+				if (cam.x + bac.x < 0) {
+					para.x++;
+					cam.x++;
+					spr.x++;
+				}
+			}
+			if (kbState[KeyEvent.VK_D]) {
+				if (cam.x + bac.x > -750) {
+					para.x--;
+					cam.x--;
+					spr.x--;
+				}
+			}
+
+			gl.glClearColor(0, 0, 0, 1);
+			gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+
+			// Parallax Animation update
+
+			if (time == 42)
+				parallexFrame = 0;
+			else if (time == 34)
+				parallexFrame = 1;
+			else if (time == 26)
+				parallexFrame = 2;
+			else if (time == 18)
+				parallexFrame = 3;
+			else if (time == 10)
+				parallexFrame = 4;
+			else if (time == 0)
+				parallexFrame = 5;
+
+			/* Draw sprite */
+
+			// Determine direction sprite is facing and update frame depending
+			// on time
+			if (faceRight) {
+				updateSpr(time, 0);
+			} else {
+				updateSpr(time, 1);
+			}
+
+			// Reset Timer
+			if (time == 0)
+				time = 50;
+			else
+				time--;
+		}
+		System.exit(0);
+	}
+
+	public static void updateSpr(int time, int dir) {
+		// Draw background
+		int xmin, ymin, xinc = 18, yinc = 14;
+		xmin = (int) Math.floor(0 - (cam.x + bac.x) / bac.w);
+		ymin = (int) Math.floor(0 - (cam.y + bac.y) / bac.w);
+		for (int i = xmin; i < xmin + xinc; i++) {
+			for (int j = ymin; j < ymin + yinc; j++) {
+				// if (tiles[i][j] == 1) {
+				// glDrawSprite(gl, background_alt, (i * 40) + bac.x + cam.x, (j
+				// * 40) + cam.y, bac.w, bac.w);
+				// } else
+				glDrawSprite(gl, background, (i * 40) + cam.x + bac.x - 40, (j * 40) + bac.y + cam.y - 40, bac.w,
+						bac.w);
+			}
+		}
+
+		// TODO: update background (it doens't work)
+		// this code just creates a static background of where the origin is
+		// at,
+		// not update to new points
+
+		// for (int i = (bac.x + cam.x); i < (bac.x + cam.x) + xwindow; i +=
+		// bac.w) {
+		// for (int j = (bac.y + cam.y); j < (bac.y + cam.y) + ywindow; j +=
+		// bac.w) {
+		// glDrawSprite(gl, background, i, j, bac.w, bac.w);
+		// }
+		// }
+
+		// Draw Parallax
+
+		if (para.x > -450 && para.x < 450 && para.y > -300 && para.y < 440)
+			glDrawSprite(gl, parallaxTex[parallexFrame], para.x, para.y, 500, 320);
+
+		// Draw player sprite
+
+		if (spr.x > -35 && spr.y > -35 && spr.y < 470 && spr.x < 630) {
+			if (dir == 0) {
+				if (time == 0) {
+					if (changedir == 0) {
+						changedir = 1;
+					} else
+						changedir = 0;
+				}
+				glDrawSprite(gl, spriteidleR[changedir], spr.x, spr.y, spriteSize[0], spriteSize[1]);
+			} else {
+				if (time == 0) {
+					if (changedir == 0) {
+						changedir = 1;
+					} else
+						changedir = 0;
+				}
+				glDrawSprite(gl, spriteidleL[changedir], spr.x, spr.y, spriteSize[0], spriteSize[1]);
+			}
+		}
+	}
+
+	// Load a file into an OpenGL texture and return that texture.
+	public static int glTexImageTGAFile(GL2 gl, String filename, int[] out_size) {
+		final int BPP = 4;
+
+		DataInputStream file = null;
+		try {
+			// Open the file.
+			file = new DataInputStream(new FileInputStream(filename));
+		} catch (FileNotFoundException ex) {
+			System.err.format("File: %s -- Could not open for reading.", filename);
+			return 0;
+		}
+
+		try {
+			// Skip first two bytes of data we don't need.
+			file.skipBytes(2);
+
+			// Read in the image type. For our purposes the image type
+			// should be either a 2 or a 3.
+			int imageTypeCode = file.readByte();
+			if (imageTypeCode != 2 && imageTypeCode != 3) {
+				file.close();
+				System.err.format("File: %s -- Unsupported TGA type: %d", filename, imageTypeCode);
+				return 0;
+			}
+
+			// Skip 9 bytes of data we don't need.
+			file.skipBytes(9);
+
+			int imageWidth = Short.reverseBytes(file.readShort());
+			int imageHeight = Short.reverseBytes(file.readShort());
+			int bitCount = file.readByte();
+			file.skipBytes(1);
+
+			// Allocate space for the image data and read it in.
+			byte[] bytes = new byte[imageWidth * imageHeight * BPP];
+
+			// Read in data.
+			if (bitCount == 32) {
+				for (int it = 0; it < imageWidth * imageHeight; ++it) {
+					bytes[it * BPP + 0] = file.readByte();
+					bytes[it * BPP + 1] = file.readByte();
+					bytes[it * BPP + 2] = file.readByte();
+					bytes[it * BPP + 3] = file.readByte();
+				}
+			} else {
+				for (int it = 0; it < imageWidth * imageHeight; ++it) {
+					bytes[it * BPP + 0] = file.readByte();
+					bytes[it * BPP + 1] = file.readByte();
+					bytes[it * BPP + 2] = file.readByte();
+					bytes[it * BPP + 3] = -1;
+				}
+			}
+
+			file.close();
+
+			// Load into OpenGL
+			int[] texArray = new int[1];
+			gl.glGenTextures(1, texArray, 0);
+			int tex = texArray[0];
+			gl.glBindTexture(GL2.GL_TEXTURE_2D, tex);
+			gl.glTexImage2D(GL2.GL_TEXTURE_2D, 0, GL2.GL_RGBA, imageWidth, imageHeight, 0, GL2.GL_BGRA,
+					GL2.GL_UNSIGNED_BYTE, ByteBuffer.wrap(bytes));
+			gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MIN_FILTER, GL2.GL_NEAREST);
+			gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MAG_FILTER, GL2.GL_NEAREST);
+
+			out_size[0] = imageWidth;
+			out_size[1] = imageHeight;
+			return tex;
+		} catch (IOException ex) {
+			System.err.format("File: %s -- Unexpected end of file.", filename);
+			return 0;
+		}
+	}
+
+	public static void glDrawSprite(GL2 gl, int tex, int x, int y, int w, int h) {
+		gl.glBindTexture(GL2.GL_TEXTURE_2D, tex);
+		gl.glBegin(GL2.GL_QUADS);
+		{
+			gl.glColor3ub((byte) -1, (byte) -1, (byte) -1);
+			gl.glTexCoord2f(0, 1);
+			gl.glVertex2i(x, y);
+			gl.glTexCoord2f(1, 1);
+			gl.glVertex2i(x + w, y);
+			gl.glTexCoord2f(1, 0);
+			gl.glVertex2i(x + w, y + h);
+			gl.glTexCoord2f(0, 0);
+			gl.glVertex2i(x, y + h);
+		}
+		gl.glEnd();
+	}
+}
+
+class Camera {
+	public int x;
+	public int y;
+}
+
+class Background {
+	public int x;
+	public int y;
+	public int w;
+}
+
+class Sprite {
+	public int x;
+	public int y;
+}
